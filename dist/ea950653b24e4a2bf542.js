0fe9998c32d2fa55dbc5ba5320b22486916a22a0
Luis Saravia <129925608+lsara011@users.noreply.github.com>
2024-09-07 14:42:14 -0400

    Delete dist directory

--- dist/ea950653b24e4a2bf542.js
+++
@@ -1,8 +0,0 @@
-import "./style.css"
-import { homePage } from "./home";
-import { menu } from "./Menu";
-import { about } from "./about";
-homePage();
-document.getElementById('home').addEventListener('click', homePage);
-document.getElementById('menu').addEventListener('click', menu);
-document.getElementById('about').addEventListener('click', about);
